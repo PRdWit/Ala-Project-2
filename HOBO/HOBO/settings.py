a9e1547648c7bf037fa0f572b0ce7a4923c1e9b2
PRdWit <P.D.business@outlook.com>
2024-05-06 12:05:17 +0200

    Started server

--- HOBO/HOBO/settings.py
+++ HOBO/HOBO/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hobo',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
