c653d34eedc280dfe2039f30c9e643f6bd6c7d7c
PRdWit <P.D.business@outlook.com>
2024-05-06 11:36:49 +0200

    Made a homepage and added it as the landing page

--- HOBO/HOBO/urls.py
+++ HOBO/HOBO/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from HoboBrain import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage, name="homepage")
 ]
