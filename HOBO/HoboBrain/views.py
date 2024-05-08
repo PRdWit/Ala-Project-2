58bf85789e78d1985846c776f452ee66eb298c21
PRdWit <P.D.business@outlook.com>
2024-05-08 10:30:53 +0200

    Fixed the db conn and displayed series

--- HOBO/HoboBrain/views.py
+++ HOBO/HoboBrain/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from HoboBrain.models import Serie
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, "homepage.html")
+    active_series = Serie.objects.filter(actief=True)
+    context = {
+        'active_series': active_series
+    }
+    return render(request, "homepage.html", context)
 
 def profile(request):
     return render(request, "profile.html")
