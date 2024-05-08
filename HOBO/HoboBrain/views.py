9d625ff841326d3fdd21d78492e61018079075f8
PRdWit <P.D.business@outlook.com>
2024-05-08 11:46:00 +0200

    Made functioning carousel

--- HOBO/HoboBrain/views.py
+++ HOBO/HoboBrain/views.py
@@ -3,14 +3,28 @@ from HoboBrain.models import Serie
 
 def homepage(request):
     active_series = Serie.objects.filter(actief=True)
+    trending_series = Serie.objects.filter(trending=True)
+    editor_series = Serie.objects.filter(editorpick=True)
 
     series_with_images = [
         {'serie': serie, 'image_name': f'{str(serie.serieid).zfill(5)}.jpg'}
         for serie in active_series
     ]
 
+    trending_series_with_images = [
+        {'serie': serie, 'image_name': f'{str(serie.serieid).zfill(5)}.jpg'}
+        for serie in trending_series
+    ]
+
+    editor_series_with_images = [
+        {'serie': serie, 'image_name': f'{str(serie.serieid).zfill(5)}.jpg'}
+        for serie in editor_series
+    ]
+
     context = {
-        'series_with_images': series_with_images
+        'series_with_images': series_with_images,
+        'trending_series_with_images' : trending_series_with_images,
+        'editor_series_with_images' : editor_series_with_images
     }
 
     return render(request, "homepage.html", context)
