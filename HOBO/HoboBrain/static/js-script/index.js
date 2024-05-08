92b8e87218594ee9974334b621cde7cd8c57de5f
StarRainArt <c.verhaar03@gmail.com>
2024-05-08 09:35:23 +0200

    working carousel for every

--- HOBO/HoboBrain/static/js-script/index.js
+++ HOBO/HoboBrain/static/js-script/index.js
@@ -9,12 +9,22 @@ let screenSize = window.innerWidth - 25;
 let maxItems = screenSize / (itemWidth);
 
 const carouselInner = document.getElementById("carousel-inner");
+const carouselInner2 = document.getElementById("carousel-inner2");
+const carouselInner3 = document.getElementById("carousel-inner3");
+
 const totalItems = carouselInner.children.length;
+const totalItems2 = carouselInner2.children.length;
+const totalItems3 = carouselInner3.children.length;
+
+let maxIndex = totalItems - maxItems;
+let maxIndex2 = totalItems2 - maxItems;
+let maxIndex3 = totalItems3 - maxItems;
 
 let index = 0;
-let maxIndex = totalItems - maxItems;
+let index2 = 0;
+let index3 = 0;
 
-function scrollCarousel(direction) {
+function carouselRecent(direction) {
   index += direction;
 
   if (index < 0) {
@@ -28,18 +38,30 @@ function scrollCarousel(direction) {
   carouselInner.style.transform = `translateX(${mover}px)`;
 }
 
-//   const visibleItems = 4;
- 
-//   const maxIndex = totalItems - visibleItems; // Maximum index where scrolling stops
-//   currentIndex += direction;
- 
-//   // Ensure the currentIndex stays within the valid range
-//   if (currentIndex < 0) {
-//     currentIndex = 0;
-//   } else if (currentIndex > maxIndex) {
-//     currentIndex = maxIndex; // Prevent overscrolling beyond the last item
-//   }
- 
-//   const translateX = -currentIndex * itemWidth; // Calculate translation
-//   carouselInner.style.transform = `translateX(${translateX}px)`;
-// }
+function carouselTrending(direction) {
+  index2 += direction;
+
+  if (index2 < 0) {
+    index2 = 0;
+  }
+  else if (index2 > maxIndex2) {
+    index2 = maxIndex2;
+  }
+
+  let mover = -index2 * itemWidth;
+  carouselInner2.style.transform = `translateX(${mover}px)`;
+}
+
+function carouselEditor(direction) {
+  index3 += direction;
+
+  if (index3 < 0) {
+    index3 = 0;
+  }
+  else if (index3 > maxIndex3) {
+    index3 = maxIndex3;
+  }
+
+  let mover = -index3 * itemWidth;
+  carouselInner3.style.transform = `translateX(${mover}px)`;
+}
