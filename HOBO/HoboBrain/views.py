a6c9cadb2fdd45e4d5a5849dca3bce2ff1fe0785
PRdWit <P.D.business@outlook.com>
2024-05-08 11:01:51 +0200

    Oogabooga

--- HOBO/HoboBrain/views.py
+++ HOBO/HoboBrain/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from HoboBrain.models import Serie
 
-# Create your views here.
-
 def homepage(request):
     active_series = Serie.objects.filter(actief=True)
+
+    series_with_images = [
+        {'serie': serie, 'image_name': f'{str(serie.serieid).zfill(5)}.jpg'}
+        for serie in active_series
+    ]
+
     context = {
-        'active_series': active_series
+        'series_with_images': series_with_images
     }
+
     return render(request, "homepage.html", context)
 
 def profile(request):
     return render(request, "profile.html")
+
