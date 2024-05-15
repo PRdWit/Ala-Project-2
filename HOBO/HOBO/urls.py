808d9347ccb2fbaf44bde09f87f589101ffb8ac3
PRdWit <P.D.business@outlook.com>
2024-05-15 13:14:12 +0200

    Fixed something in the urls

--- HOBO/HOBO/urls.py
+++ HOBO/HOBO/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from HoboBrain import views
-from HoboBrain.views import registreren, inloggen
+from HoboBrain.views import registreren
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
