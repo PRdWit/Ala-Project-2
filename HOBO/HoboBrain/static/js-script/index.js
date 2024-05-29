642799f72ce917e291da0f3431ee650a791e48f4
StarRainArt <c.verhaar03@gmail.com>
2024-05-29 11:58:58 +0200

    Recent alleen zichtbaar wnr ingelogd

--- HOBO/HoboBrain/static/js-script/index.js
+++ HOBO/HoboBrain/static/js-script/index.js
@@ -1,6 +1,6 @@
 let margin = 10;
 
-let item = document.querySelector(".carousel-item").getBoundingClientRect();
+let item = document.querySelector(".carousel-trending").getBoundingClientRect();
 let width = item.width;
 
 let itemWidth = width+margin;
@@ -9,35 +9,17 @@ let screenSize = window.innerWidth - 25;
 
 let maxItems = screenSize / (itemWidth);
 
-const carouselInner = document.getElementById("carousel-inner");
 const carouselInner2 = document.getElementById("carousel-inner2");
 const carouselInner3 = document.getElementById("carousel-inner3");
 
-const totalItems = carouselInner.children.length;
 const totalItems2 = carouselInner2.children.length;
 const totalItems3 = carouselInner3.children.length;
 
-let maxIndex = totalItems - maxItems;
 let maxIndex2 = totalItems2 - maxItems;
 let maxIndex3 = totalItems3 - maxItems;
 
-let index = 0;
 let index2 = 0;
 let index3 = 0;
-
-function carouselRecent(direction) {
-  index += direction;
-
-  if (index < 0) {
-    index = 0;
-  }
-  else if (index > maxIndex) {
-    index = maxIndex;
-  }
-
-  let mover = -index * itemWidth;
-  carouselInner.style.transform = `translateX(${mover}px)`;
-}
 
 function carouselTrending(direction) {
   index2 += direction;
